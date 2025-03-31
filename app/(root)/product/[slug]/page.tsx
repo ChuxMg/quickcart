5052112f49f4ae7d8dd0f65972408eb180be8f46
ChuxMg <chuxmgbojikwe@gmail.com>
2025-03-31 22:26:40 +0700

    Implement Add To Cart Component

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,5 +1,5 @@
-import { Badge} from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+
 import { Card, CardContent } from "@/components/ui/card";
 
 import { getProductBySlug } from "@/lib/actions/product.actions";
@@ -8,6 +8,8 @@ import { notFound } from "next/navigation";
 import ProductPrice from "@/components/shared/product/product-price";
 
 import ProductImages from "@/components/shared/product/product-images";
+
+import AddToCart from "@/components/shared/product/add-to-cart";
 
 
 const ProductDetailsPage = async (props: {
@@ -24,7 +26,9 @@ const ProductDetailsPage = async (props: {
       <section>
         <div className="grid grid-cols-1 md:grid-cols-5">
           {/* Image Colum */}
-          <div className="col-span-2"><ProductImages images={product.images} /></div>
+          <div className="col-span-2">
+            <ProductImages images={product.images} />
+          </div>
           {/* Details Column */}
           <div className="col-span-2 p-5">
             <div className="flex flex-col gap-6">
@@ -67,7 +71,16 @@ const ProductDetailsPage = async (props: {
                 </div>
                 {product.stock > 0 && (
                   <div className="flex-center">
-                    <Button className="w-full">Add To Cart</Button>
+                    <AddToCart
+                      item={{
+                        productId: product.id,
+                        name: product.name,
+                        slug: product.slug,
+                        price: product.price,
+                        qty: 1,
+                        image: product.images![0]
+                      }}
+                    />
                   </div>
                 )}
               </CardContent>
