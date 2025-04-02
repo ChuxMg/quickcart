549967a99ae2b903db1f1bda5aa8ab80bb32e29a
ChuxMg <chuxmgbojikwe@gmail.com>
2025-04-02 19:40:31 +0700

    Implement Dynamic Cart Button

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -10,7 +10,7 @@ import ProductPrice from "@/components/shared/product/product-price";
 import ProductImages from "@/components/shared/product/product-images";
 
 import AddToCart from "@/components/shared/product/add-to-cart";
-
+import { getMyCart } from "@/lib/actions/cart.actions";
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -20,6 +20,8 @@ const ProductDetailsPage = async (props: {
   const product = await getProductBySlug(slug);
 
   if (!product) notFound();
+
+  const cart = await getMyCart();
 
   return (
     <>
@@ -72,13 +74,14 @@ const ProductDetailsPage = async (props: {
                 {product.stock > 0 && (
                   <div className="flex-center">
                     <AddToCart
+                      cart={cart}
                       item={{
                         productId: product.id,
                         name: product.name,
                         slug: product.slug,
                         price: product.price,
                         qty: 1,
-                        image: product.images![0]
+                        image: product.images![0],
                       }}
                     />
                   </div>
