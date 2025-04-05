bc0ef5e31f8428c14a1076abe3db74981f3b27ef
ChuxMg <chuxmgbojikwe@gmail.com>
2025-04-05 18:49:19 +0700

    Implement Persist Session Cart

--- auth.ts
+++ auth.ts
@@ -79,6 +79,7 @@ export const config = {
     async jwt({ token, user, trigger, session }: any) {
       // Assign user fields to token
       if (user) {
+        token.id = user.id;
         token.role = user.role;
 
         // If user has no name then use the email
@@ -93,7 +94,37 @@ export const config = {
             data: { name: token.name },
           });
         }
+
+        if (trigger === "signIn" || trigger === "signUp") {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get("sessionCartId")?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              // Delete current user cart
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              });
+
+              // Assign new cart
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              });
+            }
+          }
+        }
       }
+
+      // Handle session updates
+      if (session?.user.name && trigger === "update") {
+        token.name = session.user.name;
+      }
+
       return token;
     },
 
@@ -111,12 +142,12 @@ export const config = {
         // Create new response and the new headers
         const response = NextResponse.next({
           request: {
-            headers: newRequestHeaders
-          }
-        })
+            headers: newRequestHeaders,
+          },
+        });
 
         // Set newly generated sessionCartId in the response cookies
-        response.cookies.set("sessionCartId", sessionCartId)
+        response.cookies.set("sessionCartId", sessionCartId);
 
         return response;
       } else {
