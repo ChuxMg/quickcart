acea27959b31b495632b6484a71fed5aa9b3943f
ChuxMg <chuxmgbojikwe@gmail.com>
2025-05-07 23:56:39 +0700

    Implement Review Schema, Zod & Type

--- types/index.ts
+++ types/index.ts
@@ -6,7 +6,8 @@ import {
   shippingAddressSchema,
   insertOrderItemSchema,
   insertOrderSchema,
-  paymentResultSchema
+  paymentResultSchema,
+  insertReviewSchema,
 } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -22,12 +23,18 @@ export type OrderItem = z.infer<typeof insertOrderItemSchema>;
 export type Order = z.infer<typeof insertOrderSchema> & {
   id: string;
   createdAt: Date;
-  isPaid: Boolean;
+  isPaid: boolean;
   paidAt: Date | null;
-  isDelivered: Boolean;
-  deliveredAt: Date | null
+  isDelivered: boolean;
+  deliveredAt: Date | null;
   orderitems: OrderItem[];
-  user: {name: string; email: string}
-}
+  user: { name: string; email: string };
+};
 
-export type PaymentResult = z.infer<typeof paymentResultSchema>
+export type PaymentResult = z.infer<typeof paymentResultSchema>;
+
+export type Review = z.infer<typeof insertReviewSchema> & {
+  id: string;
+  createdAt: Date;
+  user?: { name: string };
+};
